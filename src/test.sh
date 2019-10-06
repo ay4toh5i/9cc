dedc9e5a7471ede0cced84f831ede84e47ab6331
HighGreat <a.b.s2121.ayt@outlook.jp>
2019-10-06 15:33:03 +0900

    Add a tokenizer to allow space characters between tokens

--- src/test.sh
+++ src/test.sh
@@ -4,8 +4,8 @@ assert() {
   expected="$1"
   input="$2"
 
-  ./9cc "$input" > tmp.s
-  gcc -o tmp tmp.s
+  ./9cc "$input" > tmp.s || exit
+  gcc -static -o tmp tmp.s
   ./tmp
   actual="$?"
 
@@ -20,5 +20,6 @@ assert() {
 assert 0 0
 assert 42 42
 assert 21 "5+20-4"
+assert 41 " 12 + 34 - 5"
 
 echo OK
